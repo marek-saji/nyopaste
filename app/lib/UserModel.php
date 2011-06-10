ff2272fa2156f55aa6145b0cebc635f6a1809afd
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2011-06-11 00:05:47 +0200

    UserModel::exec() is now returning display name and ident fields extra

--- app/lib/UserModel.php
+++ app/lib/UserModel.php
@@ -10,8 +10,8 @@ class UserModel extends Model
     {
         parent::__construct();
 
-        static $login_len_msg  = 'Login should be 2-32 characters long';
-        static $passwd_len_msg = 'Password shoul be 3-128 characters long';
+        static $login_len_msg  = 'Login have to be 2-32 characters long';
+        static $passwd_len_msg = 'Password have to be 3-128 characters long';
 
         $this->_addField(new FId('id'));
         $this->_addField(new FString('login', true, null, 2, 32)) // UNIQUE
@@ -46,5 +46,46 @@ class UserModel extends Model
         $this->whiteListAll();
     }
 
+
+    /**
+     * Exec query, get rows.
+     *
+     * In addition to normal behaviour add [DisplayName] and [Ident] values.
+     * @author m.augustynowicz
+     *
+     * @param string $key change keys of returned array to values of this field
+     * @param bool $return do return rows
+     *
+     * @return array|void rows or nothing
+     */
+    public function exec($key=null, $return=true)
+    {
+        $result = parent::exec($key, $return);
+
+        if ($return)
+        {
+            $conf = & g()->conf['users'];
+            $aliases = array(
+                'DisplayName' => $conf['display_name_field'],
+                'Ident'       => $conf['ident_field']
+            );
+            foreach ($this->_array as &$row)
+            {
+                foreach ($aliases as $alias => $field_name)
+                {
+                    $row[$alias] =& $row[$field_name];
+                }
+            }
+
+            if ($this->_limit==1 && !empty($this->_array))
+            {
+                return ($this->_array[0]);
+            }
+            else
+            {
+                return ($this->_array);
+            }
+        }
+    }
 }
 
