9883fa2a8508ae0ba06d5330138478b97ee69766
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2011-06-13 00:09:25 +0200

    debug conf: application shortcuts and link to test environment

--- app/conf/conf.debug.php
+++ app/conf/conf.debug.php
@@ -4,6 +4,20 @@
  * @author m.augustynowicz
  */
 
-// debugs to turn on when enabling "fav"
-$conf['debug']['favorite'] = 'db, js, mail, view, debug, user, paste';
+$conf['debug'] = array(
+
+    // debugs to turn on when enabling "fav"
+    'favorite' => 'db, js, mail, view, debug, user, paste',
+
+    'alternative base URLs' => array(
+        'test' => 'http://nyopaste.test.1cm.pl/'
+    ),
+
+    'shortcuts' => array(
+        'issues'      => 'https://github.com/marek-saji/nyopaste/issues',
+        'source code' => 'https://github.com/marek-saji/nyopaste/',
+        'uservoice'   => 'http://nyopaste.uservoice.com/'
+    )
+
+);
 
