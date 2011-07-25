7baa001a6df96fe1f6f01f95f33f154fff9e7383
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2011-07-26 01:45:22 +0200

    don't add deprecated `id` attribute in paste tree

--- app/tpl/Paste/tree.php
+++ app/tpl/Paste/tree.php
@@ -2,7 +2,7 @@
     <ol>
         <?php foreach ($tree as &$paste) : ?>
             <li>
-                <?=$this->l2a($this->trans('v%s', $paste['version']), '', array($paste['url'], 'v'=>$paste['version'], 'id'=>$paste['id']))?>
+                <?=$this->l2a($this->trans('v%s', $paste['version']), '', array($paste['url'], 'v'=>$paste['version']))?>
                 <?=$this->trans('by')?>
                 <?=$this->inc('user_link', array('user'=>&$paste['paster']))?>
                 <time class="published" datetime="<?=$paste['creation']?>">
