fca4ee886d43a1545c64e08702ccb5057ead427f
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2011-10-07 23:11:29 +0200

    specify date and time formats instead of taking from locale

--- app/conf/conf.locale.php
+++ app/conf/conf.locale.php
@@ -20,13 +20,13 @@ $conf['locale'] = array(
         ),
 
         // strftime() format
-        //'human time' => '%H:%M',
-        //'human date' => '%Y-%m-%d',
-        //'human date+time' => '%Y-%m-%d at %H:%M',
-        //'sortable time' => '%H:%M',
-        //'sortable date' => '%Y-%m-%d',
+        'human time'         => '%H:%M',
+        'human date'         => '%Y-%m-%d',
+        'human date+time'    => '%Y-%m-%d at %H:%M',
+        //'sortable time'      => '%H:%M',
+        //'sortable date'      => '%Y-%m-%d',
         //'sortable date+time' => '%Y-%m-%d at %H:%M',
-        //'sql' => '%Y-$m-%dT%H:%M:%S%P'
+        //'sql'                => '%Y-$m-%dT%H:%M:%S%P'
     ),
 
     // specify regexps of accepted formats, or leave empty for all
