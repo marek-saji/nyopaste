daa39d0aca7cf10b96bbb4c6bc5ecd6e961aab69
Marek Augustynowicz <m.augustynowicz@holonglobe.com>
2012-05-30 22:48:21 +0200

    add prod to elseworlds and icons to tools

--- app/conf/conf.debug.php
+++ app/conf/conf.debug.php
@@ -10,14 +10,16 @@ $conf['debug'] = array(
     'favorite' => 'db, js, mail, view, debug, user, paste',
 
     'alternative base URLs' => array(
-        'test' => 'http://nyopaste.test.1cm.pl/'
+        'test' => 'http://nyopaste.test.1cm.pl/',
+        'prod' => 'http://nyopaste.1cm.pl/'
     ),
 
     'shortcuts' => array(
-        'issues'      => 'https://github.com/marek-saji/nyopaste/issues',
-        'source code' => 'https://github.com/marek-saji/nyopaste/',
-        'js doc'      => 'js/',
-        'uservoice'   => 'http://nyopaste.uservoice.com/'
+        '<img src="https://github.com/favicon.ico" /> issues'      => 'https://github.com/marek-saji/nyopaste/issues',
+        '<img src="https://github.com/favicon.ico" /> source code' => 'https://github.com/marek-saji/nyopaste/',
+        '<img src="http://media.tumblr.com/tumblr_ltj7t9qk9G1qbis4g.png" height=16 /> js doc'      => 'js/',
+        '<img src="http://www.uservoice.com/favicon.ico" height=16 /> uservoice'   => 'http://nyopaste.uservoice.com/',
+        '<img src="https://www.google.com/images/icons/product/analytics-16.png" /> ganalytics'  => 'https://www.google.com/analytics/web/?pli=1#report/visitors-overview/a1058361w59147407p60407824/'
     )
 
 );
